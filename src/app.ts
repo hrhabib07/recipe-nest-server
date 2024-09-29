d3336f00bd511846422899ae8334f3c7e1103d9e
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-29 11:36:22 +0600

    authentication implemented

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import routes from "./app/routes";
+import cookieParser from "cookie-parser";
+import notFound from "./app/middlewares/notFound";
+
 const app: Application = express();
-const port = 3000;
 
-// parser
-app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1", routes);
+
+//Testing
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to the Lost And Found API",
+  });
+});
+
+//global error handler
+app.use(globalErrorHandler);
+
+//handle not found
+app.use((req, res, next) => {
+  notFound(req, res, next);
 });
 
 export default app;
